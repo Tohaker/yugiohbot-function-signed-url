eb2b0190f374e70e8df6cc1a615ce3c1e2146d63
milesbardon <milesbardon@gmail.com>
2019-11-24 22:16:36 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# YuGiOhBot: Signed URL Function
+Build status: ![](https://travis-ci.org/Tohaker/yugiohbot-function-signed-url.svg?branch=master)
+
+This project sets up the Signed URL cloud function for the YuGiOhBot Submission Site.
+
+## What it does
+When called, a signed URL will be generated with a short allowed time on it, to allow the website to upload a 
+photo to Google Cloud Storage.
---
+++ yugiohbot/main.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+
+from flask import abort
+from google.cloud import storage
+
+storage_client = storage.Client()
+
+
+def function(request):
+    if request.method != 'POST':
+        return abort(405)
+
+    request_json = request.get_json()
+
+    # Get a reference to the destination file in GCS
+    bucket_name = request_json['bucket']
+    file_name = request_json['filename']
+    file = storage_client.bucket(bucket_name).blob(file_name)
+
+    # Create a temporary upload URL
+    expires_at_ms = datetime.now() + timedelta(seconds=30)
+    url = file.generate_signed_url(expires_at_ms,
+                                   content_type=request_json['contentType'])
+
+    return url
