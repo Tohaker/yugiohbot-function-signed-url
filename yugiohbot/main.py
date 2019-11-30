952e2dded443af83ce845618974a055455c439ba
milesbardon <milesbardon@gmail.com>
2019-11-30 11:14:28 +0000

    Reverted

--- yugiohbot/main.py
+++ yugiohbot/main.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timedelta
 
 from flask import abort
-from google.auth import compute_engine, default
-from google.auth.transport import requests
 from google.cloud import storage
 
 
@@ -12,23 +10,16 @@ def function(request):
 
     request_json = request.get_json()
 
-    # Allows us to generate without providing a service account file.
-    auth_request = requests.Request()
-    credentials, project = default()
-    storage_client = storage.Client(project, credentials)
+    storage_client = storage.Client()
 
     # Get a reference to the destination file in GCS
     bucket_name = request_json['bucket']
     file_name = request_json['filename']
     file = storage_client.bucket(bucket_name).blob(file_name)
 
-    signing_credentials = compute_engine.IDTokenCredentials(auth_request, "",
-                                                            service_account_email=credentials.service_account_email)
-
     # Create a temporary upload URL
     expires_at_ms = datetime.now() + timedelta(seconds=30)
     url = file.generate_signed_url(expires_at_ms,
-                                   credentials=signing_credentials,
                                    version='v4',
                                    content_type=request_json['contentType'])
 
